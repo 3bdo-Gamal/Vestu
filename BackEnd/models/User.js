6e520aac9ec92e24719f288ca496a7b7076e6e4a
Mohamed Hagag <mohamedhagag1162@gmail.com>
2025-05-04 06:10:39 +0300

    edit user schema

--- BackEnd/models/User.js
+++ BackEnd/models/User.js
@@ -1,9 +1,38 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  username: { 
+    type: String, 
+    required: true 
+  },
+  role: { 
+    type: String, 
+    enum: ["user", "admin"], 
+    default: "user" 
+  },
+  address: { 
+    type: String 
+  },
+  orders: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Order" 
+  }],
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  },
+  updatedAt: { 
+    type: Date 
+  }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
