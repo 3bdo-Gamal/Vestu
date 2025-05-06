e563addab74d85403241d93f49b05faa2bc7988e
Selwan19 <selwan21-01589@student.eelu.edu.eg>
2025-05-06 20:23:30 +0300

    tiny modification

--- vestu_homepage/server.js
+++ vestu_homepage/server.js
@@ -8,8 +8,6 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use('/images', express.static('public/images'));
 
-// Connect to MongoDB
-
 mongoose.connect('mongodb+srv://uniproject08:abc12345%23@cluster0.yubklsz.mongodb.net/?retryWrites=true&w=majority&appName=cluster0')
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
@@ -27,31 +25,25 @@ const Category = mongoose.model('Category', new mongoose.Schema({
     slug: String,
     image: String
 }));
-// === Products APIs ===
 
-// كل المنتجات
 app.get('/api/products', async (req, res) => {
     const products = await Product.find();
     res.json(products);
 });
 
-// المنتجات الـ featured
 app.get('/api/products/featured', async (req, res) => {
     const featured = await Product.find({ isFeatured: true });
     res.json(featured);
 });
 
-// === Categories APIs ===
-
-// كل الكاتيجوريز من الـ database
 app.get('/api/categories', async (req, res) => {
     const categories = await Category.find();
     res.json(categories);
 });
 
-// عشان لو الفرونت بيستخدم route ثابت فيه كاتيجوريز (زي اللي كان عندك في الأول)
+
 app.get('/api/categories/static', (req, res) => {
-    res.json(categories); // لو عندك array ثابتة برضو
+    res.json(categories);
 });
 
 app.listen(PORT, () => {
